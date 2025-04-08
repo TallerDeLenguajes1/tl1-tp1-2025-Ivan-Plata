464b2e87ca5df0b802ae2d59b9b7f1099b44da6d
Ivan-Plata <ivanplataunt@gmail.com>
2025-04-07 21:01:15 -0300

    subo punto 3,e

--- tp1_1.c
+++ tp1_1.c
@@ -5,5 +5,15 @@ int main()
 
     printf("Hola Mundo");
 
+    int num, *punt;
+
+    punt = &num;
+
+    printf("%p", punt);
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+
     return 0;
 }
